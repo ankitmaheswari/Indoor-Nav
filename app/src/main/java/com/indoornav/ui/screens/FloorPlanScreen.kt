a89e084ea43d17932073e3c8a128ac596453a89d
ankit.maheswari <ankit.maheswari@phonepe.com>
2024-02-01 23:46:51 +0530

    WIP path finder algorithm

--- app/src/main/java/com/indoornav/ui/screens/FloorPlanScreen.kt
+++ app/src/main/java/com/indoornav/ui/screens/FloorPlanScreen.kt
@@ -9,11 +9,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.indoornav.R
 
 @Composable
 fun FloorPlanScreen() {
