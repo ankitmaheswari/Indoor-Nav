7d30fdc82a6eb7be337dda91c3ca631dfc2ca515
Amit Ray <amitray769@gmail.com>
2024-02-02 04:47:03 +0530

    CustomerStoreScreen changes new

--- app/src/main/java/com/indoornav/ui/screens/customerflow/CustomerStoreScreen.kt
+++ app/src/main/java/com/indoornav/ui/screens/customerflow/CustomerStoreScreen.kt
@@ -2,13 +2,23 @@ package com.indoornav.ui.screens.customerflow
 
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -37,6 +48,7 @@ import com.indoornav.navigation.NavigationRoute
 
 
 data class QRResponse(val storeId: String, val floorId: String, val cord: Coordinate)
+
 @Composable
 fun CustomerStoreScreen(
     navController: NavHostController,
@@ -47,6 +59,10 @@ fun CustomerStoreScreen(
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val qrValue = navBackStackEntry?.arguments?.getString(NavigationRoute.QR_DATA)
     val qrResponse = gson.fromJson(qrValue, QRResponse::class.java)
+    var selectedProductId by remember {
+        mutableStateOf<String?>(null)
+    }
+
     val productList by remember {
         mutableStateOf(arrayListOf<Product>())
     }
@@ -56,7 +72,7 @@ fun CustomerStoreScreen(
     var floor by remember {
         mutableStateOf<FloorPlan?>(null)
     }
-    //todo fetch the data from firebase regarding that store and path
+
     LaunchedEffect(key1 = qrResponse.storeId, block = {
         storeDatabase.child(qrResponse.storeId).get().addOnSuccessListener {
             store = gson.fromJson<Store>(gson.toJson(it.value), Store::class.java)
@@ -66,7 +82,9 @@ fun CustomerStoreScreen(
                         gson.toJson(it.floorPlan?.get(qrResponse.floorId) ?: "{}"),
                         FloorPlan::class.java
                     )
-                } catch (e: Exception) {e.printStackTrace()}
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
         }
 
@@ -76,8 +94,13 @@ fun CustomerStoreScreen(
         productDatabase.get().addOnSuccessListener {
             val productListType = object : TypeToken<List<Product>>() {}.type
             productList.clear()
-            productList.addAll(gson.fromJson<List<Product>>(gson.toJson(ArrayList((it.getValue() as HashMap<String, HashMap<*,*>>).values)), productListType))
-        }.addOnFailureListener{
+            productList.addAll(
+                gson.fromJson<List<Product>>(
+                    gson.toJson(ArrayList((it.getValue() as HashMap<String, HashMap<*, *>>).values)),
+                    productListType
+                )
+            )
+        }.addOnFailureListener {
             Log.e("firebase", "Error getting data", it)
         }
     })
@@ -93,22 +116,45 @@ fun CustomerStoreScreen(
             }
         },
         bottomBar = {
-
+            Footer {
+                navController.popBackStack()
+            }
         }) { outerPadding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .padding(outerPadding)
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            StoreHeaderCard(navController)
+            item {
+
+                StoreHeaderCard(navController, store)
+                Text(text = "Choose the item to locate")
+            }
+            item {
+                LazyRow() {
+
+                }
+            }
+
+            items(productList.size) { item ->
+            }
+
+            productList.forEach { product ->
+                item {
+                    StoreItemCard(product){
+                        selectedProductId = it
+                    }
+                }
+            }
+
 
         }
     }
 }
 
 @Composable
-private fun StoreHeaderCard(navController: NavHostController) {
+private fun StoreHeaderCard(navController: NavHostController, store: Store?) {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -142,10 +188,61 @@ private fun StoreHeaderCard(navController: NavHostController) {
             )
             // Add your column content here
             Text("Welcome to", fontSize = 16.sp, color = Color.Black)
-            Text("", fontSize = 24.sp , color = Color.Black, fontWeight = FontWeight.Bold)
-            // ... other items
+            Text(
+                text = store?.name ?: "Retail Store",
+                fontSize = 24.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 
+}
+
+@Composable
+private fun Footer(onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .background(
+                color = Color.Green,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .clickable { onClick() }
+            .padding(
+                horizontal = 12.dp,
+                vertical = 16.dp
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.direction),
+            modifier = Modifier.padding(end = 4.dp),
+            tint = Color.Unspecified,
+            contentDescription = null
+        )
+    }
+    Text(
+        text = "Find this Item",
+        color = Color.White,
+        fontSize = 16.sp,
+        maxLines = 1
+    )
+
 
 }
+
+@Composable
+private fun StoreItemCard(product: Product, onProductSelected: (String) -> Unit) {
+    Row {
+        Image(painter = painterResource(id = R.drawable.burger), contentDescription = null)
+        Column {
+            Text(text = product.name)
+            Text(text = "₹ ${product.mrpInPaisa}")
+        }
+        RadioButton(selected = false, onClick = {
+            onProductSelected(product.productId)
+        })
+    }
+}
+
